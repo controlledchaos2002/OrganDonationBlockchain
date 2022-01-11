97c896a74c9b44e53d69c5ccbf1f6cf82c9dfd0c
zvanchy <jeevanchy@gmail.com>
2022-01-11 09:38:19 +0530

    Add function to enter the records for the patient

--- contracts/Record.sol
+++ contracts/Record.sol
@@ -39,4 +39,37 @@ contract Record {
 
 //         allRecords[_patientAddress] = Records(entries);
 //     }
+
+    struct PatientRecord{
+        string date;
+        string hospitalName;
+        string diagnosedWith;
+        string medicationProvided;
+        address diagnosedby;
+    }
+    
+    mapping(address => mapping(uint => PatientRecord)) patientRecords;
+    uint recordID =0;
+
+    function createRecord(
+        address _patientAddress,
+        string memory _date,
+        string memory _hospitalName,
+        string memory _dignosedWith,
+        string memory _medicationProvided,
+        address _diagnosedBy
+        ) public {
+            recordID ++;
+            patientRecords[_patientAddress][recordID] = PatientRecord(
+                _date,
+                _hospitalName,
+                _dignosedWith,
+                _medicationProvided,
+                _diagnosedBy
+                );
+    }
+
+    function removeRecord(uint id,address _patientAddress) public {
+        delete patientRecords[_patientAddress][id];
+    }
 }
